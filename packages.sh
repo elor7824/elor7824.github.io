4928118382cfe0ebd12f52320e2aeca448cf6fc8
Elias Ortiz <elias.ortiz@colorado.edu>
2015-07-10 13:24:40 -0600

    added all other lockglyphs

--- packages.sh
+++ packages.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+dpkg-deb -bZgzip projects/Pokemon_lockglyphs debs
+dpkg-deb -bZgzip projects/Adinkra_Lockglyph debs
+dpkg-deb -bZgzip projects/Ohio_Lockglyph debs
+dpkg-deb -bZgzip projects/Sankofa_Lockglyph debs
 dpkg-deb -bZgzip projects/Supreme_Lockglyph debs
