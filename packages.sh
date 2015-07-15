cb5abba3f95434b731e2e639cc71c9a258a56d7e
Elias Ortiz <elias.ortiz@colorado.edu>
2015-07-15 09:56:29 -0600

    added another requested lockglyph

--- packages.sh
+++ packages.sh
@@ -4,3 +4,4 @@ dpkg-deb -bZgzip projects/Adinkra_Lockglyph debs
 dpkg-deb -bZgzip projects/Ohio_Lockglyph debs
 dpkg-deb -bZgzip projects/Sankofa_Lockglyph debs
 dpkg-deb -bZgzip projects/Supreme_Lockglyph debs
+dpkg-deb -bZgzip projects/Skull_Lockglyph debs
